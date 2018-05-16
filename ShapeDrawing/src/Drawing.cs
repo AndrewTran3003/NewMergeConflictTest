1713dd5c54ed7856e50b8e5656c581de2c22e4f4
AndrewTran3003 <andrew.tran3003@gmail.com>
2018-05-17 09:53:36 +1000

    1st Contribution

---
+++ ShapeDrawing/src/Drawing.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwinGameSDK;
+namespace MyGame
+{
+    class Drawing
+    {
+        private List<Shape> _shapes;
+        private Color _background;
+        public Drawing() : this(Color.White)
+        {
+
+        }
+        public Drawing(Color _background)
+        {
+            _shapes = new List<Shape>();
+            Background = _background;
+        }
+
+        public List<Shape> SelectedShapes
+        {
+            get
+            {
+                List<Shape> result = new List<Shape>();
+                foreach (Shape s in _shapes)
+                {
+                    if (s.Selected)
+                    {
+                        result.Add(s);
+                    }
+                }
+                return result;
+            }
+        }
+
+        public int ShapeCount
+        {
+            get
+            {
+                return _shapes.Count;
+            }
+        }
+
+        public Color Background
+        {
+            get
+            {
+                return _background;
+            }
+            set
+            {
+                _background = value;
+            }
+        }
+
+        public void Draw()
+        {
+            SwinGame.ClearScreen(_background);
+            foreach (Shape s in _shapes)
+            {
+                s.Draw();
+            }
+        }
+
+        public void SelectShapesAt(Point2D pt)
+        {
+            foreach (Shape s in _shapes)
+            {
+                if (s.IsAt(pt))
+                {
+                    s.Selected = true;
+                }
+                //else
+                //{
+                //s.Selected = false;
+                //}
+            }
+        }
+
+        public void AddShape(Shape s)
+        {
+            _shapes.Add(s);
+        }
+
+        public void RemoveShape(Shape s)
+        {
+            _shapes.Remove(s);
+        }
+    }
+}
